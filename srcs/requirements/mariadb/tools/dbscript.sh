b1783ea04f69f43dbebe98a93af98c2334ca7b11
wiozsert <williams.ozserttas05@gmail.com>
2023-06-24 14:49:29 +0200

    Save, making wordpress config file

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,5 +1,8 @@
 #!bin/bash
-
+# -e : Quitte immediatement si une commande existe avec un etat != 0
+# -u : Traite les variables non definies comme une erreur lors de la substitution
+# -x : Affiche les commandes et leurs arguments au fur et a mesure de leurs execution
+#set -eux
 
 # Demarre mysql
 service mysql start;
@@ -22,10 +25,10 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 # Rafraichir pour que myqsl le prenne en compte 
 mysql -e "FLUSH PRIVILEGES;"
 
-# Redemarrage
+# Arret de mysql
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+# Demarrage securise du server mysql, cette commande ajoute des fonctionnalites
+# de securite comme le redemarrage du serveur lorsqu'une erreur a lieu.
+# De plus elle permet la journalisation des informations d'execution dans
+# un journal des erreurs
 exec mysqld_safe
-
-
-
-
---
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# -e : Quitte immediatement si une commande existe avec un etat != 0
+# -u : Traite les variables non definies comme une erreur lors de la substitution
+# -x : Affiche les commandes et leurs arguments au fur et a mesure de leurs execution
+#set -eux
+
+cd /var/www/html/wordpress
+
+if ! wp core is-installed; then
+wp config create	--allow-root --dbname=${SQL_DATABASE} \
+			--dbuser=${SQL_USER} \
+			--dbpass=${SQL_PASSWORD} \
+			--dbhost=${SQL_HOST} \
+			--url=https://${DOMAIN_NAME};
+
+wp core install	--allow-root \
+			--url=https://${DOMAIN_NAME} \
+			--title=${SITE_TITLE} \
+			--admin_user=${ADMIN_USER} \
+			--admin_password=${ADMIN_PASSWORD} \
+			--admin_email=${ADMIN_EMAIL};
+
+wp user create		--allow-root \
+			${USER1_LOGIN} ${USER1_MAIL} \
+			--role=author \
+			--user_pass=${USER1_PASS} ;
+
+wp cache flush --allow-root
+
+# Il fournit une interface facile à utiliser pour créer des formulaires de contact personnalisés
+# et gérer les soumissions, ainsi que pour prendre en charge diverses techniques anti-spam
+wp plugin install contact-form-7 --activate
+
+# Passage de la langue du site en anglais
+wp language core install en_US --activate
+
+# Suppression des theme et plugins par defaut
+wp theme delete twentynineteen twentytwenty
+wp plugin delete hello
+
+# Definie la structure du permalink
+wp rewrite structure '/%postname%/'
+
+fi
+
+if [ ! -d /run/php ]; then
+	mkdir /run/php;
+fi
+
+# Demarrage du gestionnaire de processus php fastcgi pour la version 7.3 de php au premier plan
+exec /usr/sbin/php-fpm7.3 -F -R
