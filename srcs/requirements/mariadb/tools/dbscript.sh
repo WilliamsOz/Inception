984199170fcfe62dff396f229bfc45d4a7d6e072
wiozsert <williams.ozserttas05@gmail.com>
2023-07-11 18:29:11 +0200

    Save

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -1,8 +1,4 @@
 #!bin/bash
-# -e : Quitte immediatement si une commande existe avec un etat != 0
-# -u : Traite les variables non definies comme une erreur lors de la substitution
-# -x : Affiche les commandes et leurs arguments au fur et a mesure de leurs execution
-#set -eux
 
 # Demarre mysql
 service mysql start;
@@ -10,16 +6,20 @@ service mysql start;
 # Creer la table si non existante au nom de la variable d'environnement
 # SQL_DATABASE, indique dans le fichier .env qui sera envoyer par le
 # docker-compose.yml
+echo "Creation de la BDD.."
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
+echo "Creation de l'user.."
 # Creer un utilisateur pouvant manipuler la table
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
+echo "Attribution des droits.."
 # Attribution des droits a cet utilisateur
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Modifier l'utilsateur root pour chager les droit avec le mot de passe
 # SQL_ROOT_PASSWORD
+echo "cc"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Rafraichir pour que myqsl le prenne en compte 
