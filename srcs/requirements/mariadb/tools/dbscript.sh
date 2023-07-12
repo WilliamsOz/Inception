abdeb7c6c2c61a08b85882269f4b5eac54cfd5f8
wiozsert <williams.ozserttas05@gmail.com>
2023-07-12 19:06:39 +0200

    Save

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -6,19 +6,20 @@ service mysql start;
 # Creer la table si non existante au nom de la variable d'environnement
 # SQL_DATABASE, indique dans le fichier .env qui sera envoyer par le
 # docker-compose.yml
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
 
 # Creer un utilisateur pouvant manipuler la table
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Attribution des droits a cet utilisateur
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# Modifier l'utilsateur root pour chager les droit avec le mot de passe SQL_ROOT_PASSWORD
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Attribution du mot de passe a root
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Rafraichir pour que myqsl le prenne en compte 
-mysql -e "FLUSH PRIVILEGES;"
+mysql -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 # Arret de mysql
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
